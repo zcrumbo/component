a1d3deaf6acd631643a30dde35c3e7f25d9195b5
Zachary Crumbo <zcrumbo@rei.com>
2019-05-06 12:36:55 -0700

    update build and tooling

---
+++ meta.js
@@ -0,0 +1,30 @@
+{
+  prompts: {
+    name: {
+      type: 'string',
+      required: true,
+      label: 'Project name'
+    },
+    description: {
+      type: 'string',
+      required: true,
+      label: 'Project description',
+      default: 'An REI Cedar component built on Vue.js '
+    },
+    author: {
+      type: 'string',
+      label: 'Author'
+    }
+    addComponents: {
+      type: 'boolean',
+      required: true,
+      label: 'Include sample Vue components?'
+      default: true,
+    }
+  },
+  filters: {
+    "components/*": "addComponents"
+  },
+  completeMessage: '{{#inPlace}}To get started:\n\n  npm install\n  npm run dev{{else}}To get started:\n\n  cd {{destDirName}}\n  npm install\n  npm run dev{{/inPlace}}',
+  skipInterpolation: 'src/**/*.vue'
+}
--- template/local-development.js
+++ template/local-development.js
@@ -1,5 +1,3 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 
 /* Bring In Style Sheets here for local development.
@@ -13,8 +11,8 @@ import '@rei/cdr-button/dist/cdr-button.css';
 import '@rei/cdr-icon/dist/cdr-icon.css';
 import '@rei/cdr-cta/dist/cdr-cta.css';
 
-/* Bring in entry component here */
-import SampleComponent from './SampleComponent';
+/* Bring in entry component here - from 'main' field in package.json */
+import SampleComponent from '.';
 
 Vue.config.productionTip = false;
 
