60f69f59fd36c2fb8012b26f956329b33e8a4a62
Zachary Crumbo <zcrumbo@rei.com>
2019-05-07 11:22:24 -0700

    update build and meta

--- meta.js
+++ meta.js
@@ -1,30 +1,31 @@
-{
+module.exports = {
   prompts: {
     name: {
       type: 'string',
       required: true,
-      label: 'Project name'
+      label: 'Project name',
     },
     description: {
       type: 'string',
       required: true,
       label: 'Project description',
-      default: 'An REI Cedar component built on Vue.js '
+      default: 'An REI Cedar component built on Vue.js ',
     },
     author: {
       type: 'string',
-      label: 'Author'
+      label: 'Author',
     },
     addComponents: {
       type: 'boolean',
       required: true,
       label: 'Include sample Vue components?',
       default: true,
-    }
+    },
   },
   filters: {
-    "components/*": "addComponents"
+    'src/components/**': 'addComponents',
   },
-  completeMessage: '{{#inPlace}}To get started:\n\n  npm install\n  npm run dev{{else}}To get started:\n\n  cd {{destDirName}}\n  npm install\n  npm run dev{{/inPlace}}',
-  skipInterpolation: 'src/**/*.vue'
-}
+  completeMessage:
+    '{{#inPlace}}To get started:\n\n  npm install\n  npm run dev{{else}}To get started:\n\n  cd {{destDirName}}\n  npm install\n  npm run dev{{/inPlace}}',
+  skipInterpolation: 'src/**/*.vue',
+};
--- template/src/index.js
+++ template/src/index.js
@@ -1,1 +1,3 @@
+{{#if_eq addComponents false}}/* export main component from here, e.g.:{{/if_eq}}
 export { default } from './components/SampleComponent.vue';
+{{#if_eq addComponents false}}*/{{/if_eq}}
