b96b168175e354a47c1950d2f3cb979d218f56d9
Zachary Crumbo <zcrumbo@rei.com>
2019-05-09 17:35:29 -0700

    update docs and add build files

---
+++ template/src/components/Component.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>{{title}}</h1>
+    <p>{{description}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainComponent',
+  props: ['title','description'],
+}
+</script>
+
+<style lang="scss">
+@import '../globals.scss';
+</style>
